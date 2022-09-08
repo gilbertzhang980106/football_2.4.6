fd892ae357cc2956983a1eebcefbad0bb5dcfbdc
Gilbert <gilbertzhang980106@gmail.com>
2022-09-08 13:22:33 +0800

    界面调整&&头像尺寸修改为50*50

--- assets/Script/component/netCom.ts
+++ assets/Script/component/netCom.ts
@@ -9,7 +9,7 @@ const {ccclass, property} = cc._decorator;
 export default class netCom extends cc.Component {   
 
     logonData = {
-        token : "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjI2MzMxLCJhdWQiOiIiLCJleHAiOjE2NjUwNDcyNzgsImlhdCI6MTY2MjQ1NTI3OCwiaXNzIjoiIiwianRpIjoiYTljYzEyMzhlY2MwZDg2NDhhMGNjN2UwODQ4MGQ4MTEiLCJuYmYiOjE2NjI0NTUyNzgsInN1YiI6IiJ9.ITQC7zbvDV8OpUrTfO3o69STngyxbg30Q90UH6FaNGw",
+        token : "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1aWQiOjI2MzMxLCJhdWQiOiIiLCJleHAiOjE2NjUxOTgwNTYsImlhdCI6MTY2MjYwNjA1NiwiaXNzIjoiIiwianRpIjoiZTA1NDk1MDM1YzRlMDU5YzY5ZTM2YTYxM2NiMWZkMDAiLCJuYmYiOjE2NjI2MDYwNTYsInN1YiI6IiJ9.srFfIj36gt8rOYs0a8j2iciQSD7FvuYaWq6ghTmlYiE",
     }
 
     static instance:netCom = null;
